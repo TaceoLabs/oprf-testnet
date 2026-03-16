8ee50a4a0d64171acbb3a5386b9ffd64c9b2820b
Anyplace44 <guggi@taceo.io>
2026-03-16 09:44:03 +0100

    .

--- build/socat.sh
+++ build/socat.sh
@@ -14,7 +14,14 @@ ip link set lo up
 # Verify it's up
 ip addr show lo
 
+# listen to http connections from outside
 socat VSOCK-LISTEN:8000,fork,keepalive TCP:127.0.0.1:8000,keepalive &
+
+# forward db request from the enclave to outside
+socat TCP-LISTEN:5432,bind=0.0.0.0,fork,reuseaddr,keepalive VSOCK-CONNECT:21:5432,keepalive
+
+# forward rpc requests from the enclave to outside
+socat TCP-LISTEN:443,bind=0.0.0.0,fork,reuseaddr,keepalive VSOCK-CONNECT:21:443,keepalive
 
 set -a && . /app/.env && set +a
 echo "before sleep"
