c34c84f814e16613f211ce9615ad997041d1a479
Anyplace44 <guggi@taceo.io>
2026-03-17 11:29:45 +0100

    .

--- build/socat.sh
+++ build/socat.sh
@@ -7,8 +7,8 @@ echo "start script"
 # Create minimal /etc/hosts so getaddrinfo can resolve 0.0.0.0 / 127.0.0.1
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "127.0.0.2 oprf-tee-testnet-2-cluster-prod.cluster-c1i26k0aa2nn.eu-central-1.rds.amazonaws.com" >> /etc/hosts
-echo "127.0.0.3 alchemy.com" >> /etc/hosts
-# echo "127.0.0.3 opt-mainnet.g.alchemy.com" >> /etc/hosts
+# echo "127.0.0.3 alchemy.com" >> /etc/hosts
+echo "127.0.0.3 opt-mainnet.g.alchemy.com" >> /etc/hosts
 # Create minimal nsswitch.conf so glibc knows to check /etc/hosts
 echo "hosts: files" > /etc/nsswitch.conf
 cat /etc/hosts
@@ -62,6 +62,6 @@ sleep 15
 ls
 ls -l /app/
 echo "before starting oprf"
-/app/taceo-oprf-testnet-node
+/app/taceo-oprf-testnet-node || true
 echo "exiting in 100 seconds..."
 sleep 100
