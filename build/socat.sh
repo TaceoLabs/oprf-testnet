ca01f257751f0c92803c58b710203e7b1dfa9389
Anyplace44 <guggi@taceo.io>
2026-03-17 10:28:49 +0100

    .

--- build/socat.sh
+++ build/socat.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 set -e
 
-echo "sleep 20..."
+echo "sleep 5..."
 sleep 10
 echo "start script"
 # Create minimal /etc/hosts so getaddrinfo can resolve 0.0.0.0 / 127.0.0.1
 echo "127.0.0.1 localhost" > /etc/hosts
+echo "127.0.0.2 oprf-tee-testnet-2-cluster-prod.cluster-c1i26k0aa2nn.eu-central-1.rds.amazonaws.com" > /etc/hosts
 
 # Create minimal nsswitch.conf so glibc knows to check /etc/hosts
 echo "hosts: files" > /etc/nsswitch.conf
@@ -15,21 +16,41 @@ ip link set lo up
 # Verify it's up
 ip addr show lo
 
-echo "Waiting for 8000 port"
+echo "Ensure loopback addresses exist"
+if ! ip addr show dev lo | grep -q "127.0.0.2"; then
+  ip addr add 127.0.0.2/32 dev lo:0
+  ip link set dev lo:0 up
+fi
+if ! ip addr show dev lo | grep -q "127.0.0.200"; then
+  ip addr add 127.0.0.200/32 dev lo:0
+  ip link set dev lo:0 up
+fi
+sleep 5
+
+
+# echo "Start vsock proxies"
+# # Connections from Enclave
+# vscproxy -parentCID=3 -vsockPort=5432 -localAddr=127.0.0.2:5432 &
+sleep 5
+
+
+# echo "Forward 5432 port"
+# # forward db request from the enclave to outside
+socat TCP-LISTEN:5432,bind=127.0.0.2,fork,reuseaddr,keepalive VSOCK-CONNECT:3:5432,keepalive &
+#
+# echo "Forward 443 port"
+# # forward rpc requests from the enclave to outside
+# socat TCP-LISTEN:443,bind=0.0.0.0,fork,reuseaddr,keepalive VSOCK-CONNECT:21:443,keepalive &
+
+
+
+echo "Accepting outside connection on port 4563"
 # listen to http connections from outside
-socat VSOCK-LISTEN:8000,fork,keepalive TCP:127.0.0.1:4563,keepalive &
-
-echo "Forward 5432 port"
-# forward db request from the enclave to outside
-socat TCP-LISTEN:5432,bind=0.0.0.0,fork,reuseaddr,keepalive VSOCK-CONNECT:21:5432,keepalive &
-
-echo "Forward 443 port"
-# forward rpc requests from the enclave to outside
-socat TCP-LISTEN:443,bind=0.0.0.0,fork,reuseaddr,keepalive VSOCK-CONNECT:21:443,keepalive &
+socat VSOCK-LISTEN:4563,fork,keepalive TCP:127.0.0.1:4563,keepalive &
 
 set -a && . /app/.env && set +a
 echo "before sleep"
-sleep 100
+sleep 5
 ls
 ls -l /app/
 echo "before starting oprf"
